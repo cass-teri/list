cb7792dbcfb215f1888a15b4cf43c3b90dc4ecbd
Cassandra Thompson <cassandra.t.thompson@gov.ab.ca>
2024-09-01 18:45:08 -0600

    feature: adding dynamic file name size so it doesn't wrap so bad on smaller displays.

---
+++ README.md
@@ -0,0 +1,11 @@
+# List
+
+---
+
+A super simple directory list app that has icons. 
+
+requires a Nerdfonts compatible font to be installed in your terminal or console.
+
+---
+
+![Screenshot](img.png)
--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,32 @@ use human_bytes::human_bytes;
 use std::fs::{self, DirEntry, Metadata};
 use std::path::Path;
 use tabular::{Row, Table};
+use terminal_size::{Width, Height, terminal_size};
+
+
+
+
 
 fn main() {
+    let size = terminal_size();
+    let mut max_width = 320;
+    if let Some((Width(w), Height(_))) = size {
+        if w < 26 {
+            max_width = 1;
+        }
+        else {
+            max_width = (w - 26u16) as usize;
+        }
+    } else {
+        println!("Unable to get terminal size");
+    }
+
+
     let path = std::env::current_dir().unwrap();
     let items = list_path(&path);
 
     match items {
-        Some(item) => display_paths(item, path.to_str().unwrap().to_string()),
+        Some(item) => display_paths(item, path.to_str().unwrap().to_string(), max_width),
         None => eprintln!("Failed to read directory"),
     };
 }
@@ -24,7 +43,7 @@ fn list_path(path: &Path) -> Option<Vec<DirEntry>> {
     Some(items)
 }
 
-fn display_paths(items: Vec<DirEntry>, root_path: String) {
+fn display_paths(items: Vec<DirEntry>, root_path: String, max_width: usize) {
     println!("Reading {root_path} items[{:?}]", items.len());
 
     let mut table = Table::new("{:<}  {:<}  {:<} {:<}");
@@ -35,10 +54,14 @@ fn display_paths(items: Vec<DirEntry>, root_path: String) {
         let file_type_icon = get_file_type_icon_from_metadata(&metadata);
         let accessed: DateTime<Utc> = metadata.accessed().unwrap().into();
 
+        let file_name = item.file_name();
+        let file_name = file_name.to_str().unwrap();
+        let file_name = file_name.chars().take(max_width).collect::<String>();
+
         table.add_row(
             Row::new()
                 .with_cell(file_type_icon)
-                .with_cell(item.file_name().to_str().unwrap().to_owned().white())
+                .with_cell(file_name.clone().to_owned().white())
                 .with_cell(format!("{}", accessed.format("%F")).bright_black())
                 .with_cell(human_bytes(metadata.len() as f64).red()),
         );
